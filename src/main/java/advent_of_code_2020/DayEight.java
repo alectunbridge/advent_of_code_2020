3ea1ed8113e46cc67c6443eae41e6a584f677993
alec-tunbridge <alec.tunbridge@autotrader.co.uk>
2020-12-11 21:25:22 +0000

    Day eight part 2

--- src/main/java/advent_of_code_2020/DayEight.java
+++ src/main/java/advent_of_code_2020/DayEight.java
@@ -10,20 +10,38 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//1652 too high
 public class DayEight {
     public static void main(String[] args) throws URISyntaxException, IOException {
         Path path = Paths.get(DayOne.class.getClassLoader()
                 .getResource("day_eight.txt").toURI());
 
         List<String> instructions = Files.readAllLines(path);
+        long answer = 0;
+        for (int i = 0; i < instructions.size(); i++) {
+            String originalInstruction = instructions.get(i);
+            if (originalInstruction.matches("jmp.*")) {
+                System.out.println("replacing line: " + i);
+                instructions.set(i,originalInstruction.replace("jmp", "nop"));
+                try {
+                    answer = runProgram(instructions);
+                    break;
+                } catch (RuntimeException re) {
+                    System.out.println(re.getMessage());
+                    instructions.set(i, originalInstruction);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static long runProgram(List<String> instructions) {
 
         long acc = 0;
         Set<Integer> visitedInstructions = new HashSet<>();
-
         for (int instructionPointer = 0, linesSize = instructions.size(); instructionPointer < linesSize; instructionPointer++) {
             if(visitedInstructions.contains(instructionPointer)){
-                break;
+                throw new RuntimeException("Infinite loop detected");
             }
             visitedInstructions.add(instructionPointer);
             String line = instructions.get(instructionPointer);
@@ -60,6 +78,6 @@ public class DayEight {
                 }
             }
         }
-        System.out.println(acc);
+        return acc;
     }
 }
