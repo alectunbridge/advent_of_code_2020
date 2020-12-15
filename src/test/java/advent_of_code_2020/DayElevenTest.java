81bb96ea15fdd54816ad67b57aa55d6957219124
alec.tunbridge <alec.tunbridge@gmail.com>
2020-12-15 22:27:12 +0000

    Day Eleven - part 1 solved

--- src/test/java/advent_of_code_2020/DayElevenTest.java
+++ src/test/java/advent_of_code_2020/DayElevenTest.java
@@ -2,13 +2,18 @@ package advent_of_code_2020;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DayElevenTest {
     @Test
     void testExampleRound1() {
         DayEleven dayEleven = new DayEleven(
-                   "L.LL.LL.LL" + "\n" +
+                "L.LL.LL.LL" + "\n" +
                         "LLLLLLL.LL" + "\n" +
                         "L.L.L..L.." + "\n" +
                         "LLLL.LL.LL" + "\n" +
@@ -23,7 +28,7 @@ public class DayElevenTest {
         String output = dayEleven.nextRound();
 
         assertThat(output).isEqualTo(
-                        "#.##.##.##" + "\n" +
+                "#.##.##.##" + "\n" +
                         "#######.##" + "\n" +
                         "#.#.#..#.." + "\n" +
                         "####.##.##" + "\n" +
@@ -39,7 +44,7 @@ public class DayElevenTest {
     @Test
     void testExampleRound2() {
         DayEleven dayEleven = new DayEleven(
-                   "#.##.##.##" + "\n" +
+                "#.##.##.##" + "\n" +
                         "#######.##" + "\n" +
                         "#.#.#..#.." + "\n" +
                         "####.##.##" + "\n" +
@@ -54,16 +59,16 @@ public class DayElevenTest {
         String output = dayEleven.nextRound();
 
         assertThat(output).isEqualTo(
-                       "#.LL.L#.##" + "\n" +
-                       "#LLLLLL.L#" + "\n" +
-                       "L.L.L..L.." + "\n" +
-                       "#LLL.LL.L#" + "\n" +
-                       "#.LL.LL.LL" + "\n" +
-                       "#.LLLL#.##" + "\n" +
-                       "..L.L....." + "\n" +
-                       "#LLLLLLLL#" + "\n" +
-                       "#.LLLLLL.L" + "\n" +
-                       "#.#LLLL.##"
+                "#.LL.L#.##" + "\n" +
+                        "#LLLLLL.L#" + "\n" +
+                        "L.L.L..L.." + "\n" +
+                        "#LLL.LL.L#" + "\n" +
+                        "#.LL.LL.LL" + "\n" +
+                        "#.LLLL#.##" + "\n" +
+                        "..L.L....." + "\n" +
+                        "#LLLLLLLL#" + "\n" +
+                        "#.LLLLLL.L" + "\n" +
+                        "#.#LLLL.##"
         );
     }
 
@@ -71,30 +76,30 @@ public class DayElevenTest {
     void testExampleRound3() {
         DayEleven dayEleven = new DayEleven(
                 "#.LL.L#.##" + "\n" +
-                     "#LLLLLL.L#" + "\n" +
-                     "L.L.L..L.." + "\n" +
-                     "#LLL.LL.L#" + "\n" +
-                     "#.LL.LL.LL" + "\n" +
-                     "#.LLLL#.##" + "\n" +
-                     "..L.L....." + "\n" +
-                     "#LLLLLLLL#" + "\n" +
-                     "#.LLLLLL.L" + "\n" +
-                     "#.#LLLL.##"
+                        "#LLLLLL.L#" + "\n" +
+                        "L.L.L..L.." + "\n" +
+                        "#LLL.LL.L#" + "\n" +
+                        "#.LL.LL.LL" + "\n" +
+                        "#.LLLL#.##" + "\n" +
+                        "..L.L....." + "\n" +
+                        "#LLLLLLLL#" + "\n" +
+                        "#.LLLLLL.L" + "\n" +
+                        "#.#LLLL.##"
         );
 
         String output = dayEleven.nextRound();
 
         assertThat(output).isEqualTo(
-                       "#.##.L#.##" + "\n" +
-                       "#L###LL.L#" + "\n" +
-                       "L.#.#..#.." + "\n" +
-                       "#L##.##.L#" + "\n" +
-                       "#.##.LL.LL" + "\n" +
-                       "#.###L#.##" + "\n" +
-                       "..#.#....." + "\n" +
-                       "#L######L#" + "\n" +
-                       "#.LL###L.L" + "\n" +
-                       "#.#L###.##"
+                "#.##.L#.##" + "\n" +
+                        "#L###LL.L#" + "\n" +
+                        "L.#.#..#.." + "\n" +
+                        "#L##.##.L#" + "\n" +
+                        "#.##.LL.LL" + "\n" +
+                        "#.###L#.##" + "\n" +
+                        "..#.#....." + "\n" +
+                        "#L######L#" + "\n" +
+                        "#.LL###L.L" + "\n" +
+                        "#.#L###.##"
         );
     }
 
@@ -117,4 +122,11 @@ public class DayElevenTest {
 
         assertThat(occupiedSeats).isEqualTo(37);
     }
+
+    @Test
+    public void answerPart1() throws URISyntaxException, IOException {
+        assertThat(new DayEleven(Files.readString(Path.of(this.getClass().getClassLoader().getResource("day_eleven.txt").toURI())))
+                .findNumberOfOccupiedSeats()).isEqualTo(-1);
+    }
+
 }
