3558589834ad191048edd96f0866b5eff7a0aa3e
alec.tunbridge <alec.tunbridge@gmail.com>
2020-12-18 22:14:02 +0000

    Day Thirteen Part 1 - complete

--- src/main/java/advent_of_code_2020/DayThirteen.java
+++ src/main/java/advent_of_code_2020/DayThirteen.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 
 public class DayThirteen {
 
-    private int result;
+    private int arrivalTime;
     private List<Integer> buses;
 
     public DayThirteen(String input) {
         String[] lines = input.split("\n");
-        result = Integer.parseInt(lines[0]);
+        arrivalTime = Integer.parseInt(lines[0]);
         buses = Arrays.stream(lines[1].split(","))
                 .filter(field ->!"x".equals(field))
                 .map(Integer::parseInt)
@@ -21,13 +21,31 @@ public class DayThirteen {
     public static void main(String[] args) {
         DayThirteen dayThirteen = new DayThirteen("1004098\n" +
                 "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,509,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,401,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19");
+        System.out.println(dayThirteen.getAnswer());
     }
 
-    public int getResult() {
-        return result;
+    public int getArrivalTime() {
+        return arrivalTime;
     }
 
     public List<Integer> getBuses() {
         return buses;
+    }
+
+    public int getAnswer() {
+        int shortestWaitingTime = Integer.MAX_VALUE;
+        int bestBus = -1;
+        for (int bus: buses) {
+            int waitingTime = (arrivalTime / bus + 1) * bus - arrivalTime;
+            if(waitingTime == 0) {
+                return bus;
+            } else if(waitingTime<shortestWaitingTime){
+                shortestWaitingTime = waitingTime;
+                bestBus = bus;
+            }
+        }
+        System.out.println(bestBus);
+        System.out.println(shortestWaitingTime);
+        return shortestWaitingTime * bestBus;
     }
 }
--- src/test/java/advent_of_code_2020/DayThirteenTest.java
+++ src/test/java/advent_of_code_2020/DayThirteenTest.java
@@ -10,7 +10,8 @@ class DayThirteenTest {
     void testPart1Example() {
         DayThirteen dayThirteen = new DayThirteen("939\n" +
                 "7,13,x,x,59,x,31,19");
-        assertThat(dayThirteen.getResult()).isEqualTo(939);
+        assertThat(dayThirteen.getArrivalTime()).isEqualTo(939);
         assertThat(dayThirteen.getBuses()).containsExactly(7,13,59,31,19);
+        assertThat(dayThirteen.getAnswer()).isEqualTo(295);
     }
 }
