8fefcc3622907753c6f95d29d5f2402bc4f9260e
alec-tunbridge <alec.tunbridge@autotrader.co.uk>
2020-12-23 21:47:53 +0000

    Day Fourteen Part 2 - refactor to keep both parts working

--- src/main/java/advent_of_code_2020/DayFourteen.java
+++ src/main/java/advent_of_code_2020/DayFourteen.java
@@ -16,41 +16,17 @@ public class DayFourteen {
     private char[] mask;
     private Map<Long,Long> memory;
 
-    public DayFourteen(String... input) {
+    public DayFourteen(MemInstructionExecutor memInstructionExecutor, String... input) {
         memory = new HashMap<>();
         mask = new char[36];
         Arrays.fill(mask,'X');
         for (String line : input) {
             if ("mem".equals(line.substring(0, 3))) {
-                executeMemInstruction(line);
+                memInstructionExecutor.execute(line, mask, memory);
             } else {
                 this.mask = line.substring(7).toCharArray();
             }
         }
-    }
-
-    private void executeMemInstruction(String memInstruction) {
-        Pattern pattern = Pattern.compile("mem\\[(\\d*)] = (\\d*)");
-        Matcher matcher = pattern.matcher(memInstruction);
-        if(!matcher.matches()){
-            throw new IllegalArgumentException("Can't parse mem instruction: " + memInstruction);
-        }
-        long bitmaskedValue = applyMask(Long.parseLong(matcher.group(2)));
-        memory.put(Long.parseLong(matcher.group(1)),bitmaskedValue);
-    }
-
-    private long applyMask(long value) {
-        char[] paddedValueAsBits = String.format("%36s", Long.toBinaryString(value))
-                .replace(' ', '0').toCharArray();
-
-        for (int i = 0, charArrayLength = mask.length; i < charArrayLength; i++) {
-            char maskBit = mask[i];
-            if ('X' == maskBit) {
-                continue;
-            }
-            paddedValueAsBits[i] = mask[i];
-        }
-        return Long.parseUnsignedLong(String.valueOf(paddedValueAsBits),2);
     }
 
     public String getMask() {
@@ -61,13 +37,46 @@ public class DayFourteen {
         return memory.get(address);
     }
 
+    private Map<Long,Long> getMemory() {
+        return memory;
+    }
+
     public long sumMemoryContents() {
         return memory.values().stream().mapToLong(Long::longValue).sum();
     }
 
     public static void main(String[] args) throws URISyntaxException, IOException {
         List<String> input = Files.readAllLines(Path.of(DayFourteen.class.getClassLoader().getResource("day_fourteen.txt").toURI()));
-        DayFourteen dayFourteen = new DayFourteen(input.toArray(new String[0]));
+        DayFourteen dayFourteen = new DayFourteen(new MemInstructionExecutorPart1(), input.toArray(new String[0]));
         System.out.println(dayFourteen.sumMemoryContents());
+    }
+
+    public static class MemInstructionExecutorPart1 implements MemInstructionExecutor {
+
+        @Override
+        public void execute(String memInstruction, char[] mask, Map<Long, Long> memory) {
+            Pattern pattern = Pattern.compile("mem\\[(\\d*)] = (\\d*)");
+            Matcher matcher = pattern.matcher(memInstruction);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("Can't parse mem instruction: " + memInstruction);
+            }
+            long bitmaskedValue = applyMask(Long.parseLong(matcher.group(2)), mask);
+            memory.put(Long.parseLong(matcher.group(1)), bitmaskedValue);
+        }
+
+        @Override
+        public long applyMask(long value, char[] mask) {
+            char[] paddedValueAsBits = String.format("%36s", Long.toBinaryString(value))
+                    .replace(' ', '0').toCharArray();
+
+            for (int i = 0, charArrayLength = mask.length; i < charArrayLength; i++) {
+                char maskBit = mask[i];
+                if ('X' == maskBit) {
+                    continue;
+                }
+                paddedValueAsBits[i] = mask[i];
+            }
+            return Long.parseUnsignedLong(String.valueOf(paddedValueAsBits),2);
+        }
     }
 }
--- src/test/java/advent_of_code_2020/DayFourteenTest.java
+++ src/test/java/advent_of_code_2020/DayFourteenTest.java
@@ -2,24 +2,25 @@ package advent_of_code_2020;
 
 import org.junit.jupiter.api.Test;
 
+import static advent_of_code_2020.DayFourteen.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DayFourteenTest {
     @Test
     void parseMask() {
-        DayFourteen dayFourteen = new DayFourteen("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+        DayFourteen dayFourteen = new DayFourteen(new MemInstructionExecutorPart1(), "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
         assertThat(dayFourteen.getMask()).isEqualTo("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
     }
 
     @Test
     void parseWriteOperation() {
-        DayFourteen dayFourteen = new DayFourteen("mem[36932] = 186083");
+        DayFourteen dayFourteen = new DayFourteen(new MemInstructionExecutorPart1(), "mem[36932] = 186083");
         assertThat(dayFourteen.getLocation(36932)).isEqualTo(186083);
     }
 
     @Test
     void parseWriteWithMask() {
-        DayFourteen dayFourteen = new DayFourteen("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+        DayFourteen dayFourteen = new DayFourteen(new MemInstructionExecutorPart1(), "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
         "mem[8] = 11");
 
         assertThat(dayFourteen.getLocation(8)).isEqualTo(73);
@@ -27,7 +28,7 @@ class DayFourteenTest {
 
     @Test
     void testPartOneExample() {
-        DayFourteen dayFourteen = new DayFourteen("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+        DayFourteen dayFourteen = new DayFourteen(new MemInstructionExecutorPart1(), "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
                 "mem[8] = 11",
                 "mem[7] = 101",
                 "mem[8] = 0");
