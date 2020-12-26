c35fd72a5f0c44335f264fac13f7a9f53bc77903
alectunbridge <alec.tunbridge@gmail.com>
2020-12-26 20:19:52 +0000

    Day 15 part 2

--- src/main/java/advent_of_code_2020/DayFifteen.java
+++ src/main/java/advent_of_code_2020/DayFifteen.java
@@ -1,44 +1,52 @@
 package advent_of_code_2020;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.IntPredicate;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 class DayFifteen {
 
     private List<Integer> numbers;
     private int index;
+    private int lastNumber;
+    private Map<Integer,Integer> cache = new HashMap<>();
 
     public DayFifteen(Integer... numbers) {
         this.numbers = Arrays.stream(numbers).collect(Collectors.toList());
     }
 
-    public static void main(String[] args) {
-
-    }
-
     int takeTurn() {
+        int newEntry = 0;
         if(index < numbers.size()){
+            cache.put(numbers.get(index), index);
             return numbers.get(index++);
         }
-        int lastNumber = numbers.get(numbers.size()-1);
-        for (int i = numbers.size()-2; i >= 0; i--) {
-            if(numbers.get(i) == lastNumber){
-                int newNumber = index -1 - i;
-                index++;
-                numbers.add(newNumber);
-                return newNumber;
-            }
+        lastNumber = numbers.get(numbers.size()-1);
+        Integer lastIndex = cache.get(lastNumber);
+        if(lastIndex == null || lastIndex == index-1){
+            //do nowt
+        } else {
+            newEntry = index - lastIndex - 1;
         }
+        numbers.add(newEntry);
+        cache.put(lastNumber,index-1);
         index++;
-        numbers.add(0);
-        return 0;
+        return newEntry;
     }
 
 	public int find2020thNumber() {
         int answer = 0 ;
         for (int i = 0; i < 2020; i++) {
+            answer = takeTurn();
+        }
+		return answer;
+	}
+
+	public int find30_000_000thNumber() {
+        int answer = 0 ;
+        for (int i = 0; i < 30_000_000; i++) {
             answer = takeTurn();
         }
 		return answer;
--- src/test/java/advent_of_code_2020/DayFifteenTest.java
+++ src/test/java/advent_of_code_2020/DayFifteenTest.java
@@ -5,10 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class DayFifteenTest {
-    
+
     @Test
-    void exampleTest(){
-        DayFifteen dayFifteen = new DayFifteen(0,3,6);
+    void exampleTest() {
+        DayFifteen dayFifteen = new DayFifteen(0, 3, 6);
 
         assertThat(dayFifteen.takeTurn()).isEqualTo(0);
         assertThat(dayFifteen.takeTurn()).isEqualTo(3);
@@ -23,52 +23,92 @@ public class DayFifteenTest {
     }
 
     @Test
-    void example2020thNumber0_3_6(){
-        DayFifteen dayFifteen = new DayFifteen(0,3,6);
+    void example2020thNumber0_3_6() {
+        DayFifteen dayFifteen = new DayFifteen(0, 3, 6);
 
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(436);
     }
 
     @Test
-    void example2020thNumbers1_3_2(){
-        DayFifteen dayFifteen = new DayFifteen(1,3,2);
+    void example2020thNumbers1_3_2() {
+        DayFifteen dayFifteen = new DayFifteen(1, 3, 2);
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(1);
     }
 
     @Test
-    void example2020thNumbers2_1_3(){
-        DayFifteen dayFifteen = new DayFifteen(2,1,3);
+    void example2020thNumbers2_1_3() {
+        DayFifteen dayFifteen = new DayFifteen(2, 1, 3);
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(10);
     }
 
     @Test
-    void example2020thNumbers1_2_3(){
-        DayFifteen dayFifteen = new DayFifteen(1,2,3);
+    void example2020thNumbers1_2_3() {
+        DayFifteen dayFifteen = new DayFifteen(1, 2, 3);
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(27);
     }
 
     @Test
-    void example2020thNumbers2_3_1(){
-        DayFifteen dayFifteen = new DayFifteen(2,3,1);
+    void example2020thNumbers2_3_1() {
+        DayFifteen dayFifteen = new DayFifteen(2, 3, 1);
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(78);
     }
 
     @Test
-    void example2020thNumbers3_2_1(){
-        DayFifteen dayFifteen = new DayFifteen(3,2,1);
+    void example2020thNumbers3_2_1() {
+        DayFifteen dayFifteen = new DayFifteen(3, 2, 1);
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(438);
     }
 
     @Test
-    void example2020thNumbers3_1_2(){
-        DayFifteen dayFifteen = new DayFifteen(3,1,2);
+    void example2020thNumbers3_1_2() {
+        DayFifteen dayFifteen = new DayFifteen(3, 1, 2);
         assertThat(dayFifteen.find2020thNumber()).isEqualTo(1836);
     }
 
     @Test
-    void partOneAnswer(){
-        DayFifteen dayFifteen = new DayFifteen(2,0,6,12,1,3);
-        assertThat(dayFifteen.find2020thNumber()).isEqualTo(0);
+    void partOneAnswer() {
+        DayFifteen dayFifteen = new DayFifteen(2, 0, 6, 12, 1, 3);
+        assertThat(dayFifteen.find2020thNumber()).isEqualTo(1428);
     }
 
+    @Test
+    void partTwoExample() {
+        DayFifteen dayFifteen = new DayFifteen(0, 3, 6);
+        assertThat(dayFifteen.find30_000_000thNumber()).isEqualTo(175594);
+    }
+
+    @Test
+    void partTwoExample1() {
+        assertThat(new DayFifteen(1, 3, 2).find30_000_000thNumber()).isEqualTo(2578);
+    }
+
+    @Test
+    void partTwoExample2() {
+        assertThat(new DayFifteen(2, 1, 3).find30_000_000thNumber()).isEqualTo(3544142);
+    }
+
+    @Test
+    void partTwoExample3() {
+        assertThat(new DayFifteen(1, 2, 3).find30_000_000thNumber()).isEqualTo(261214);
+    }
+
+    @Test
+    void partTwoExample4() {
+        assertThat(new DayFifteen(2, 3, 1).find30_000_000thNumber()).isEqualTo(6895259);
+    }
+
+    @Test
+    void partTwoExample5() {
+        assertThat(new DayFifteen(3, 1, 2).find30_000_000thNumber()).isEqualTo(362);
+    }
+
+    @Test
+    void partTwoExample6() {
+        assertThat(new DayFifteen(3, 2, 1).find30_000_000thNumber()).isEqualTo(18);
+    }
+
+    @Test
+    void partTwoAnswer(){
+        assertThat(new DayFifteen(2,0,6,12,1,3).find30_000_000thNumber()).isEqualTo(3718541);
+    }
 }
