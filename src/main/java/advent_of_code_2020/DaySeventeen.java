60ae1afbca95fffef43c029669fd900ad7f1b6c6
alec-tunbridge <alec.tunbridge@autotrader.co.uk>
2020-12-29 21:56:00 +0000

    Day Seventeen Part 1 - done - refactored to remove maps

--- src/main/java/advent_of_code_2020/DaySeventeen.java
+++ src/main/java/advent_of_code_2020/DaySeventeen.java
@@ -3,15 +3,13 @@ package advent_of_code_2020;
 import org.apache.commons.lang3.SerializationUtils;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class DaySeventeen {
-    private TreeMap<Integer,char[][]> cube = new TreeMap<>();
+    private char[][][] cube = new char[1][][];
 
     public DaySeventeen(String... input) {
-        cube.put(0, new char[input.length][]);
-        char[][] chars = cube.get(0);
+        cube[0] = new char[input.length][];
+        char[][] chars = cube[0];
         for (int i = 0; i < input.length; i++) {
             chars[i] = input[i].toCharArray();
         }
@@ -20,38 +18,45 @@ public class DaySeventeen {
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        cube.forEach((key, value) -> {
-            result.append("z="+key+"\n");
-            for (int i = 0; i < value.length; i++) {
-                for (int j = 0; j < value[i].length; j++) {
-                    result.append(value[i][j]);
+        for (int z = 0; z < cube.length; z++) {
+            char[][] chars = cube[z];
+            result.append("z=" + (z - cube.length/2) + "\n");
+            for (int i = 0; i < cube[z].length; i++) {
+                for (int j = 0; j < cube[z][i].length; j++) {
+                    result.append(cube[z][i][j]);
                 }
                 result.append('\n');
             }
-        });
+        }
+        ;
         return result.toString();
     }
 
-    public int countNeighbours(int layer, int row, int column) {
+    public int countNeighbours(int z, int row, int column) {
         int result = 0;
-        for (int l = layer-1; l <= layer+1 ; l++) {
-            char[][] chars = cube.get(l);
-            if (chars == null) {
-                continue;
-            }
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    if(i==0 && j==0 && l==layer){
-                        continue;
-                    }
-                    try {
-                        if ('#' == chars[row + i][column + j]) {
-                            result++;
+        for (int l = z - 1; l <= z + 1; l++) {
+            try {
+                char[][] chars = cube[l];
+                if (chars == null) {
+                    continue;
+                }
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        if (i == 0 && j == 0 && l == z) {
+                            continue;
                         }
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        //do nowt
+                        try {
+                            if ('#' == chars[row + i][column + j]) {
+                                result++;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            //do nowt
+                        }
+
                     }
                 }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                //do nowt
             }
         }
         return result;
@@ -59,27 +64,27 @@ public class DaySeventeen {
 
     public void cycle() {
         growCube();
-        TreeMap<Integer,char[][]> copyOfCube = copyCube();
+        char[][][] copyOfCube = copyCube();
         writeNewCubeStates(copyOfCube);
         cube = copyOfCube;
     }
 
-    private void writeNewCubeStates(TreeMap<Integer, char[][]> copyOfCube) {
-        for (Map.Entry<Integer, char[][]> layer : copyOfCube.entrySet()) {
-            for (int i = 0; i < layer.getValue().length ; i++) {
-                for (int j = 0; j < layer.getValue().length; j++) {
-                    int neighbourCount = countNeighbours(layer.getKey(), i, j);
-                    if(layer.getValue()[i][j] == '#'){
-                        if(neighbourCount == 2 || neighbourCount == 3){
-                            copyOfCube.get(layer.getKey())[i][j] = '#';
+    private void writeNewCubeStates(char[][][] copyOfCube) {
+        for (int z = 0; z < cube.length; z++) {
+            for (int i = 0; i < cube[z].length; i++) {
+                for (int j = 0; j < cube[z].length; j++) {
+                    int neighbourCount = countNeighbours(z, i, j);
+                    if (cube[z][i][j] == '#') {
+                        if (neighbourCount == 2 || neighbourCount == 3) {
+                            copyOfCube[z][i][j] = '#';
                         } else {
-                            copyOfCube.get(layer.getKey())[i][j] = '.';
+                            copyOfCube[z][i][j] = '.';
                         }
                     } else {
-                        if(neighbourCount == 3){
-                            copyOfCube.get(layer.getKey())[i][j] = '#';
+                        if (neighbourCount == 3) {
+                            copyOfCube[z][i][j] = '#';
                         } else {
-                            copyOfCube.get(layer.getKey())[i][j] = '.';
+                            copyOfCube[z][i][j] = '.';
                         }
                     }
                 }
@@ -87,36 +92,45 @@ public class DaySeventeen {
         }
     }
 
-    private TreeMap<Integer, char[][]> copyCube() {
+    private char[][][] copyCube() {
         return SerializationUtils.clone(cube);
     }
 
     private void growCube() {
-        //grow each layer
-        int newSize = cube.get(0).length + 2;
-        for (Map.Entry<Integer, char[][]> layer : cube.entrySet()) {
+        //grow in x-y
+        int newSize = cube[0].length + 2;
+        for (int z = 0; z < cube.length; z++) {
             char[][] largerLayer = new char[newSize][newSize];
             for (char[] chars : largerLayer) {
-                Arrays.fill(chars,'.');
+                Arrays.fill(chars, '.');
             }
-            for (int i = 0; i < layer.getValue().length; i++) {
-                for (int j = 0; j < layer.getValue().length; j++) {
-                    largerLayer[i+1][j+1] = layer.getValue()[i][j];
+            for (int i = 0; i < cube[z].length; i++) {
+                for (int j = 0; j < cube[z].length; j++) {
+                    largerLayer[i + 1][j + 1] = cube[z][i][j];
                 }
             }
-            cube.put(layer.getKey(),largerLayer);
+            cube[z] = largerLayer;
+        }
+
+        //grow in z
+        int newZsize = cube.length + 2;
+        //shift existing z-layers up one
+        char[][][] newZ = new char[newZsize][][];
+        for( int i=0; i< cube.length; i++) {
+            newZ[i+1] = cube[i];
         }
 
         char[][] newBottomLayer = new char[newSize][newSize];
-        for(char[] chars: newBottomLayer) {
+        for (char[] chars : newBottomLayer) {
             Arrays.fill(chars, '.');
         }
-        cube.put(cube.firstKey()-1,newBottomLayer);
+        newZ[0] = newBottomLayer;
 
         char[][] newTopLayer = new char[newSize][newSize];
-        for(char[] chars: newTopLayer){
-            Arrays.fill(chars,'.');
+        for (char[] chars : newTopLayer) {
+            Arrays.fill(chars, '.');
         }
-        cube.put(cube.lastKey()+1,newTopLayer);
+        newZ[newZsize - 1] = newTopLayer;
+        cube = newZ;
     }
 }
