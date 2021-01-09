575e214ee0a1d13004b02e4179e7ef8337f05568
alec.tunbridge <alec.tunbridge@gmail.com>
2021-01-09 22:50:04 +0000

    DayNineteen Part1 - far too slow for actual input needs thought

--- src/main/java/advent_of_code_2020/DayNineteen.java
+++ src/main/java/advent_of_code_2020/DayNineteen.java
@@ -1,44 +1,60 @@
 package advent_of_code_2020;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 
 public class DayNineteen {
 
+    private final List<String> inputStrings;
     private List<Rule> rules;
+    private String masterRule;
 
     public DayNineteen(String... rulesAsStrings) {
         Pattern pattern = Pattern.compile("(\\d+): (.*)");
         rules = new ArrayList<>();
+        inputStrings = new ArrayList<>();
         for (String ruleAsString : rulesAsStrings) {
             Matcher matcher = pattern.matcher(ruleAsString);
-            if (!matcher.matches()) {
-                throw new IllegalArgumentException("Can't parse rule from input: " + ruleAsString);
+            if (matcher.matches()) {
+                rules.add(new Rule(matcher.group(1), matcher.group(2).replaceAll("\"", "")));
+            } else {
+                if (ruleAsString.matches("")) {
+                    //do nowt
+                } else if (ruleAsString.matches("[ab]+")) {
+                    inputStrings.add(ruleAsString);
+                } else {
+                    throw new IllegalArgumentException("Can't parse rule from input: " + ruleAsString);
+                }
             }
-            rules.add(new Rule(matcher.group(1), matcher.group(2).replaceAll("\"", "")));
         }
         rules.sort(Comparator.comparing(Rule::getRuleNo));
     }
 
     public String getRegex() {
-        String masterRule = rules.get(0).getRegex();
-        while (masterRule.matches(".*\\d.*")){
-            String[] ruleNumbers = masterRule.split(" ");
-            Arrays.sort(ruleNumbers);
-            for (int i = ruleNumbers.length-1; i>=0; i--) {
-                String ruleNumber = ruleNumbers[i];
-                if (ruleNumber.matches("\\d+")) {
-                    masterRule = masterRule.replaceAll(ruleNumber, "( "+rules.get(parseInt(ruleNumber)).getRegex()+" )");
+        if(masterRule == null) {
+            masterRule = rules.get(0).getRegex();
+            int count = 0;
+            while (masterRule.matches(".*\\d.*")) {
+                count++;
+                System.out.println(count);
+                String[] ruleNumbers = masterRule.split(" ");
+                Arrays.sort(ruleNumbers);
+                for (int i = ruleNumbers.length - 1; i >= 0; i--) {
+                    String ruleNumber = ruleNumbers[i];
+                    if (ruleNumber.matches("\\d+")) {
+                        masterRule = masterRule.replaceAll(ruleNumber, "( " + rules.get(parseInt(ruleNumber)).getRegex() + " )");
+                    }
                 }
             }
+            return masterRule.replaceAll(" ", "");
         }
-        return masterRule.replaceAll(" ","");
+        return masterRule.replaceAll(" ", "");
+    }
+
+    public long getCountOfMatchingStrings() {
+        return inputStrings.stream().filter(s->s.matches(getRegex())).count();
     }
 }
--- src/test/java/advent_of_code_2020/DayNineteenTest.java
+++ src/test/java/advent_of_code_2020/DayNineteenTest.java
@@ -1,7 +1,11 @@
 package advent_of_code_2020;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,13 +37,50 @@ public class DayNineteenTest {
     void part1Example2() {
         DayNineteen dayNineteen = new DayNineteen(
                 "0: 4 1 5",
-                        "1: 2 3 | 3 2",
-                        "2: 4 4 | 5 5",
-                        "3: 4 5 | 5 4",
-                        "4: \"a\"",
-                        "5: \"b\""
+                "1: 2 3 | 3 2",
+                "2: 4 4 | 5 5",
+                "3: 4 5 | 5 4",
+                "4: \"a\"",
+                "5: \"b\""
         );
         assertThat(dayNineteen.getRegex()).isEqualTo("(a)(((a)(a)|(b)(b))((a)(b)|(b)(a))|((a)(b)|(b)(a))((a)(a)|(b)(b)))(b)");
+    }
 
+    @Test
+    void parseRulesAndInputStrings() {
+        DayNineteen dayNineteen = new DayNineteen(
+                "0: \"a\"",
+                "",
+                "a",
+                "b");
+        assertThat(dayNineteen.getRegex()).isEqualTo("a");
+        assertThat(dayNineteen.getCountOfMatchingStrings()).isEqualTo(1L);
+    }
+
+    @Test
+    void part1ExampleCountValidStrings() {
+        DayNineteen dayNineteen = new DayNineteen(
+                "0: 4 1 5",
+                "1: 2 3 | 3 2",
+                "2: 4 4 | 5 5",
+                "3: 4 5 | 5 4",
+                "4: \"a\"",
+                "5: \"b\"",
+                "",
+                "ababbb",
+                "bababa",
+                "abbbab",
+                "aaabbb",
+                "aaaabbb"
+        );
+
+        assertThat(dayNineteen.getCountOfMatchingStrings()).isEqualTo(2L);
+    }
+
+    @Test
+    void part1Answer() throws URISyntaxException, IOException {
+        String[] lines = Files.readAllLines(Path.of(this.getClass().getClassLoader().getResource("day_nineteen.txt").toURI())).toArray(new String[0]);
+        DayNineteen dayNineteen = new DayNineteen(lines);
+        assertThat(dayNineteen.getCountOfMatchingStrings()).isEqualTo(0L);
     }
 }
