2c1a8106efa9449b1a90202288ec3e8b78c2defd
alec-tunbridge <alec.tunbridge@autotrader.co.uk>
2021-01-19 13:41:55 +0000

    Day Twenty - Part 1 - store tiles in multimap keyed by edges

--- src/test/java/advent_of_code_2020/DayTwenty.java
+++ src/test/java/advent_of_code_2020/DayTwenty.java
@@ -1,18 +1,30 @@
 package advent_of_code_2020;
 
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static org.apache.commons.lang3.StringUtils.*;
 
 class Tile {
     private final int id;
+    private final String[] lines;
     private String topRow;
     private String bottomRow;
     private String leftEdge;
     private String rightEdge;
 
-    public Tile(String... lines){
+
+    public Tile(String... lines) {
+        this.lines = lines;
         Matcher matcher = Pattern.compile("^Tile (\\d+):$").matcher(lines[0]);
-        if(!matcher.matches()){
+        if (!matcher.matches()) {
             throw new IllegalArgumentException("Can't parse id from: " + lines[0]);
         }
         id = Integer.parseInt(matcher.group(1));
@@ -27,6 +39,11 @@ class Tile {
         }
         leftEdge = leftEdgeBuilder.toString();
         rightEdge = rightEdgeBuilder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(lines).collect(Collectors.joining("\n"));
     }
 
     public int getId() {
@@ -52,4 +69,23 @@ class Tile {
 
 public class DayTwenty {
 
+    private MultiValuedMap<String, Tile> tileMap = new HashSetValuedHashMap<>();
+
+    public void addTile(Tile tile) {
+        tileMap.put(tile.getTopRow(), tile);
+        tileMap.put(reverse(tile.getTopRow()), tile);
+
+        tileMap.put(tile.getRightEdge(), tile);
+        tileMap.put(reverse(tile.getRightEdge()), tile);
+
+        tileMap.put(tile.getBottomRow(), tile);
+        tileMap.put(reverse(tile.getBottomRow()), tile);
+
+        tileMap.put(tile.getLeftEdge(), tile);
+        tileMap.put(reverse(tile.getLeftEdge()), tile);
+    }
+
+    public Collection<Tile> getTileByEdge(String edge) {
+        return tileMap.get(edge);
+    }
 }
--- src/test/java/advent_of_code_2020/DayTwentyTest.java
+++ src/test/java/advent_of_code_2020/DayTwentyTest.java
@@ -1,7 +1,9 @@
 package advent_of_code_2020;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
+import static org.apache.commons.lang3.StringUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DayTwentyTest {
@@ -27,5 +29,36 @@ public class DayTwentyTest {
         assertThat(tile.getBottomRow()).isEqualTo("....#...##");
         assertThat(tile.getLeftEdge()).isEqualTo(".#..#...#.");
         assertThat(tile.getRightEdge()).isEqualTo("####.#.###");
+    }
+
+    @Test
+    void hashTiles() {
+        DayTwenty dayTwenty = new DayTwenty();
+        Tile tile = new Tile(
+                "Tile 3469:",
+                ".##..#.#.#",
+                "##..#...##",
+                "...#..##.#",
+                "....#.#..#",
+                "#..#.##...",
+                ".#.##.#.##",
+                "..#.#.....",
+                "..###.#..#",
+                "#.##.#...#",
+                "....#...##"
+        );
+        dayTwenty.addTile(tile);
+
+        assertThat(dayTwenty.getTileByEdge(".##..#.#.#")).containsExactly(tile);
+        assertThat(dayTwenty.getTileByEdge(reverse(".##..#.#.#"))).containsExactly(tile);
+
+        assertThat(dayTwenty.getTileByEdge("####.#.###")).containsExactly(tile);
+        assertThat(dayTwenty.getTileByEdge(reverse("####.#.###"))).containsExactly(tile);
+
+        assertThat(dayTwenty.getTileByEdge("....#...##")).containsExactly(tile);
+        assertThat(dayTwenty.getTileByEdge(reverse("....#...##"))).containsExactly(tile);
+
+        assertThat(dayTwenty.getTileByEdge(".#..#...#.")).containsExactly(tile);
+        assertThat(dayTwenty.getTileByEdge(reverse(".#..#...#."))).containsExactly(tile);
     }
 }
