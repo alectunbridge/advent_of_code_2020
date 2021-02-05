77b3cc1db542ae3bed69f944826264e92817091a
alec.tunbridge <alec.tunbridge@gmail.com>
2021-02-05 14:40:44 +0000

    DayTwentyOne - Part 1 - gets first foodstuff by allergen

--- src/main/java/advent_of_code_2020/DayTwentyOne.java
+++ src/main/java/advent_of_code_2020/DayTwentyOne.java
@@ -1,5 +1,8 @@
 package advent_of_code_2020;
 
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,18 +11,22 @@ import java.util.regex.Pattern;
 
 public class DayTwentyOne {
 
-    private final Set<String> ingredients;
+    private MultiValuedMap<String, Set<String>> ingredients = new HashSetValuedHashMap<>();
 
-    public DayTwentyOne(String food) {
+    public DayTwentyOne(String... foods) {
         Pattern foodPattern = Pattern.compile("(.*)\\(contains (.*)\\)");
-        Matcher foodMatcher = foodPattern.matcher(food);
-        if(!foodMatcher.matches()){
-            throw new IllegalArgumentException("Unable to parse food: " + food);
+
+        for (String food : foods) {
+
+            Matcher foodMatcher = foodPattern.matcher(food);
+            if(!foodMatcher.matches()){
+                throw new IllegalArgumentException("Unable to parse food: " + food);
+            }
+            ingredients.put(foodMatcher.group(2), new HashSet<>(Arrays.asList(foodMatcher.group(1).split(" "))));
         }
-        ingredients = new HashSet<>(Arrays.asList(foodMatcher.group(1).split(" ")));
     }
 
-    public Set<String> getIngredientsByAllergen(String dairy) {
-        return ingredients;
+    public Set<String> getIngredientsByAllergen(String allergen) {
+        return ingredients.get(allergen).iterator().next();
     }
 }
--- src/test/java/advent_of_code_2020/DayTwentyOneTest.java
+++ src/test/java/advent_of_code_2020/DayTwentyOneTest.java
@@ -7,9 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DayTwentyOneTest {
     @Test
-    void createMap() {
-        assertThat(new DayTwentyOne("trh fvjkl sbzzf mxmxvkd (contains dairy)")
+    void getIngredientsByAllergen() {
+        DayTwentyOne dayTwentyOne = new DayTwentyOne(
+                "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+                "sqjhc fvjkl (contains soy)"
+        );
+        assertThat(dayTwentyOne
                 .getIngredientsByAllergen("dairy"))
                 .containsOnly("trh", "fvjkl", "sbzzf", "mxmxvkd");
+
+        assertThat(dayTwentyOne
+                .getIngredientsByAllergen("soy"))
+                .containsOnly("sqjhc", "fvjkl");
     }
 }
