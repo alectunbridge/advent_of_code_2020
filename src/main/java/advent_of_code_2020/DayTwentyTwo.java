5cf78c6984411f5a8abf23d0437163ed047c1168
alec.tunbridge <alec.tunbridge@gmail.com>
2021-02-13 16:32:58 +0000

    DayTwentyTwo - Part 1 - completes game - doesn't score

---
+++ src/main/java/advent_of_code_2020/DayTwentyTwo.java
@@ -0,0 +1,64 @@
+package advent_of_code_2020;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DayTwentyTwo {
+
+    private Deque<Integer> playerOneDeck;
+    private Deque<Integer> playerTwoDeck;
+
+    public DayTwentyTwo(String... lines) {
+        playerOneDeck = new ArrayDeque<>();
+        playerTwoDeck = new ArrayDeque<>();
+
+        Deque<Integer> activeDeck = null;
+        for (String line : lines) {
+            if ("Player 1:".equals(line)) {
+                activeDeck = playerOneDeck;
+            } else if ("Player 2:".equals(line)) {
+                activeDeck = playerTwoDeck;
+            } else if (!line.isBlank()) {
+                activeDeck.add(Integer.valueOf(line));
+            }
+        }
+    }
+
+    public int drawPlayer1() {
+        return playerOneDeck.remove();
+    }
+
+    public int drawPlayer2() {
+        return playerTwoDeck.remove();
+    }
+
+    public boolean playRound() {
+        Integer playerOneCard = drawPlayer1();
+        Integer playerTwoCard = drawPlayer2();
+
+        if (playerOneCard > playerTwoCard) {
+            playerOneDeck.addLast(playerOneCard);
+            playerOneDeck.addLast(playerTwoCard);
+        } else {
+            playerTwoDeck.addLast(playerTwoCard);
+            playerTwoDeck.addLast(playerOneCard);
+        }
+        return playerOneDeck.isEmpty()||playerTwoDeck.isEmpty();
+    }
+
+    public Deque<Integer> getPlayerOneDeck() {
+        return playerOneDeck;
+    }
+
+    public Deque<Integer> getPlayerTwoDeck() {
+        return playerTwoDeck;
+    }
+
+    public int completeGame() {
+        int roundCount = 0;
+        while (playRound()) {
+            roundCount++;
+        }
+        return roundCount;
+    }
+}
---
+++ src/test/java/advent_of_code_2020/DayTwentyTwoTest.java
@@ -0,0 +1,108 @@
+package advent_of_code_2020;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DayTwentyTwoTest {
+    @Test
+    void draw() {
+        DayTwentyTwo dayTwentyTwo = new DayTwentyTwo(
+                "Player 1:",
+                "9",
+                "2",
+                "6",
+                "3",
+                "1",
+                "",
+                "Player 2:",
+                "5",
+                "8",
+                "4",
+                "7",
+                "10"
+        );
+        assertThat(dayTwentyTwo.drawPlayer1()).isEqualTo(9);
+        assertThat(dayTwentyTwo.getPlayerOneDeck()).containsExactly(2, 6, 3, 1);
+        assertThat(dayTwentyTwo.drawPlayer2()).isEqualTo(5);
+        assertThat(dayTwentyTwo.getPlayerTwoDeck()).containsExactly(8, 4, 7, 10);
+    }
+
+    @Test
+    void playARoundPlayerOneWins() {
+        DayTwentyTwo dayTwentyTwo = new DayTwentyTwo(
+                "Player 1:",
+                "9",
+                "2",
+                "6",
+                "3",
+                "1",
+                "",
+                "Player 2:",
+                "5",
+                "8",
+                "4",
+                "7",
+                "10"
+        );
+
+        dayTwentyTwo.playRound();
+
+        assertThat(dayTwentyTwo.getPlayerOneDeck()).containsExactly(
+                2, 6, 3, 1, 9, 5
+        );
+        assertThat(dayTwentyTwo.getPlayerTwoDeck()).containsExactly(
+                8, 4, 7, 10
+        );
+    }
+
+    @Test
+    void playARoundPlayerTwoWins() {
+        DayTwentyTwo dayTwentyTwo = new DayTwentyTwo(
+                "Player 1:",
+                "2",
+                "6",
+                "3",
+                "1",
+                "9",
+                "5",
+                "",
+                "Player 2:",
+                "8",
+                "4",
+                "7",
+                "10"
+        );
+
+        dayTwentyTwo.playRound();
+
+        assertThat(dayTwentyTwo.getPlayerOneDeck()).containsExactly(
+                6, 3, 1, 9, 5
+        );
+        assertThat(dayTwentyTwo.getPlayerTwoDeck()).containsExactly(
+                4, 7, 10, 8, 2
+        );
+    }
+
+    @Test
+    void playGame() {
+        DayTwentyTwo dayTwentyTwo = new DayTwentyTwo(
+                "Player 1:",
+                "9",
+                "2",
+                "6",
+                "3",
+                "1",
+                "",
+                "Player 2:",
+                "5",
+                "8",
+                "4",
+                "7",
+                "10"
+        );
+
+        assertThat(dayTwentyTwo.completeGame() == 29);
+    }
+
+}
