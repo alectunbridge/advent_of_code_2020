6a796ecadc00eac65db649f6fb8ccefa8c742cfa
alec.tunbridge <alec.tunbridge@gmail.com>
2021-11-14 12:39:06 +0000

    DayTwentyThree - example works
    WIP

--- src/main/java/advent_of_code_2020/DayTwentyThree.java
+++ src/main/java/advent_of_code_2020/DayTwentyThree.java
@@ -4,17 +4,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DayTwentyThree {
     private List<Integer> cups;
+    private int currentCupIndex;
 
     public DayTwentyThree(Integer... cups) {
         this.cups = new ArrayList<>();
         this.cups.addAll(Arrays.asList(cups));
+        currentCupIndex = 0;
     }
 
     @Override
     public String toString() {
-        return cups.stream().map(Object::toString).collect(Collectors.joining(" "));
+        return IntStream
+                .range(0, cups.size())
+                .mapToObj(i -> i == currentCupIndex ? "*"+cups.get(i) : cups.get(i).toString())
+                .collect(Collectors.joining(" "));
+    }
+
+    public void move() {
+        List<Integer> removedCups = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            int indexToRemove = currentCupIndex + 1;
+            if(indexToRemove == cups.size()) {
+                indexToRemove = 0;
+                currentCupIndex--;
+            }
+            removedCups.add(cups.remove(indexToRemove));
+        }
+        int destinationCup = cups.get(currentCupIndex) - 1;
+        if(destinationCup == 0) {
+            destinationCup = cups.size()+removedCups.size();
+        }
+        while(removedCups.contains(destinationCup)) {
+            destinationCup--;
+            if(destinationCup == 0) {
+                destinationCup = cups.size()+removedCups.size();
+            }
+        }
+
+        int insertionIndex = cups.indexOf(destinationCup)+1;
+        if(insertionIndex<currentCupIndex) {
+            currentCupIndex+=removedCups.size();
+        }
+        cups.addAll(insertionIndex, removedCups);
+        currentCupIndex++;
+        currentCupIndex %= cups.size();
     }
 }
--- src/test/java/advent_of_code_2020/DayTwentyThreeTest.java
+++ src/test/java/advent_of_code_2020/DayTwentyThreeTest.java
@@ -6,8 +6,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DayTwentyThreeTest {
     @Test
-    void name() {
+    void stringRepresentation() {
         DayTwentyThree dayTwentyThree = new DayTwentyThree(3,8,9,1,2,5,4,6,7);
-        assertThat(dayTwentyThree).asString().isEqualTo("3 8 9 1 2 5 4 6 7");
+        assertThat(dayTwentyThree).asString().isEqualTo("*3 8 9 1 2 5 4 6 7");
+    }
+
+    @Test
+    void move() {
+        DayTwentyThree dayTwentyThree = new DayTwentyThree(3,8,9,1,2,5,4,6,7);
+
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("3 *2 8 9 1 5 4 6 7");
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("3 2 *5 4 6 7 8 9 1");
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("3 4 6 7 2 5 *8 9 1");
+
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("*4 6 7 9 1 3 2 5 8");
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("4 *1 3 6 7 9 2 5 8");
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("4 1 *9 3 6 7 2 5 8");
+
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("4 1 9 *2 5 8 3 6 7");
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("4 1 5 8 3 9 2 *6 7");
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("*5 7 4 1 8 3 9 2 6");
+
+        dayTwentyThree.move();
+        assertThat(dayTwentyThree).asString().isEqualTo("5 *8 3 7 4 1 9 2 6");
     }
 }
